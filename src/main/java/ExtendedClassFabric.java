555c47c5835e5cdb1d313bd1ca3a707d06ad3a62
Oleg Ivanov <o.ivanov@g.nsu.ru>
2023-03-16 14:43:01 +0700

    Inherited the say method

--- src/main/java/ExtendedClassFabric.java
+++ src/main/java/ExtendedClassFabric.java
@@ -1,15 +1,32 @@
 import io.activej.codegen.ClassBuilder;
 import io.activej.codegen.DefiningClassLoader;
+
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import static io.activej.codegen.expression.Expressions.*;
 
+
 public class ExtendedClassFabric {
-    public Object[] createClass(Class<?> aCLass, ArrayList<Object[]> parameters) {
+
+    public ExtendedClassFabric(){
+    }
+    // чтобы нормально добавлять методы в билдер класса делаем билдер для Expressionov
+    private class ActiveJExpessionBuilder{
+
+    }
+
+    public Object createObject(Class<?> aCLass, ArrayList<Object[]> parameters) {
         ArrayList<Class<?>[]> parametersTypes = new ArrayList<>();
+        Class<?> aInterface = null;
+        for (int i = 0; i < aCLass.getInterfaces().length; i++) {
+            if (aCLass.getInterfaces()[i].isAnnotationPresent(RootInterface.class)) {
+                aInterface = aCLass.getInterfaces()[i];
+            }
+        }
+        assert aInterface != null;
+
         for (int i = 0; i < parameters.size(); i++) {
             Class<?>[] array = new Class[parameters.get(i).length];
             parametersTypes.add(i, array);
@@ -17,45 +34,38 @@ public class ExtendedClassFabric {
                 array[j] = parameters.get(i)[j].getClass();
             }
         }
-        Object[] compositionObjects = new Object[aCLass.getAnnotation(ExtendsAll.class).classes().length + 1];
+        ArrayList<SomeInterface> compositionObjects = new ArrayList<>(aCLass.getAnnotation(ExtendsAll.class).classes().length);
         try {
-            compositionObjects[0] = aCLass.getConstructor(parametersTypes.get(0)).newInstance(parameters.get(0));
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            if(parametersTypes.size()>0)
+                compositionObjects.add(0, (SomeInterface) aCLass.getConstructor(parametersTypes.get(0)).newInstance(parameters.get(0)));
+            else
+                compositionObjects.add(0, (SomeInterface) aCLass.getConstructor().newInstance());
+        }
+
+        catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             System.out.println(Arrays.toString(parametersTypes.get(0)));
             e.printStackTrace();
-
         }
+
         for (int i = 0; i < aCLass.getAnnotation(ExtendsAll.class).classes().length; i++) {
             try {
                 if (i + 1 >= parameters.size())
-                    compositionObjects[i + 1] = aCLass.getAnnotation(ExtendsAll.class).classes()[i].getConstructor().newInstance();
+                    compositionObjects.add(i+1, (SomeInterface) aCLass.getAnnotation(ExtendsAll.class).classes()[i].getConstructor().newInstance());
                 else
-                    compositionObjects[i + 1] = aCLass.getAnnotation(ExtendsAll.class).classes()[i].getConstructor(parametersTypes.get(i + 1)).newInstance(parameters.get(i + 1));
+                    compositionObjects.add(i+1, (SomeInterface) aCLass.getAnnotation(ExtendsAll.class).classes()[i].getConstructor(parametersTypes.get(i + 1)).newInstance(parameters.get(i + 1)));
             } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
                 e.printStackTrace();
             }
         }
-        DefiningClassLoader classLoader = DefiningClassLoader.create();
-        Class aInterface = null;
-        for (int i = 0; i < aCLass.getInterfaces().length; i++) {
-            if (aCLass.getInterfaces()[i].isAnnotationPresent(RootInterface.class)) {
-                aInterface = aCLass.getInterfaces()[i];
-            }
-        }
-        if (aInterface == null) {
-            try {
-                throw new ClassNotFoundException("Extended class must implement the root interface");
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-        for (Method method : aInterface.getMethods()) {
 
-        }
-        Class<SomeInterface> rootInterfaceClass = ClassBuilder.create(SomeInterface.class)
-                .withMethod("say", call(staticField(System.class, "out"), "println", value("Hello world")))
-                .defineClass(classLoader);
-        return compositionObjects;
+        ClassBuilder<?> builder = ClassBuilder.create(aCLass);
+        builder=builder.withField("objects",compositionObjects.getClass(),value(compositionObjects));
+        builder=builder.withMethod("say",call(cast(call(property(self(),"objects"),"get",value(1)),SomeInterface.class)/*вот ето будет в билдере expressions*/,"say"));
+        builder=builder.withMethod("say",call(cast(call(property(self(),"objects"),"get",value(2)),SomeInterface.class),"say"));
+        DefiningClassLoader classLoader = DefiningClassLoader.create();
+        return builder.defineClassAndCreateInstance(classLoader);
+        //for (Method method : aInterface.getMethods()) {
+            //}
     }
 }
 
